38c11ed0535816e46166d81432af258c70b5ca0b
Remy Broun <remybroun@yahoo.co.uk>
2022-08-03 21:22:38 +0100

    Initial commit

---
+++ app/Http/Controllers/FormController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Notifications\SendEmail;
+use Illuminate\Support\Facades\Notification;
+
+class FormController extends Controller
+{
+    public function index(Request $request){
+        dd($request->all());
+    }
+    public function store(Request $request){
+        Notification::route('mail', [
+            'remy@broun.fr' => 'Remy Broun',
+        ])->notify(new SendEmail($request->all()));
+
+
+        dd($request->all());
+    }
+}
